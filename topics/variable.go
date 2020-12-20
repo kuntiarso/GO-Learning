4ebc67b1112e86c2c422cbeb4856e99d64de98e8
Kuntiarso <gilang.grk17@gmail.com>
2020-12-20 11:53:02 +0700

    variable | GO

---
+++ topics/variable.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// 1st way
+	var hello string
+	var profit int
+	hello = "Hello World"
+	profit = 28733
+	fmt.Println(hello)
+	fmt.Println(profit)
+
+	fmt.Println()
+
+	// 2nd way
+	var hello2 = "Hello Indonesia"
+	var profit2 = 65564
+	fmt.Println(hello2)
+	fmt.Println(profit2)
+
+	fmt.Println()
+
+	// 3rd way
+	hello3 := "Hello Canada"
+	profit3 := 11233
+	fmt.Println(hello3)
+	fmt.Println(profit3)
+
+	fmt.Println()
+}
